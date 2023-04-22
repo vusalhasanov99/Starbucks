f758db313ea4d5e8e8dda18138db8dc16221c91e
vusalhasanov99 <vusqa1999@gmail.com>
2023-04-22 20:11:30 +0400

    Login with localhost

--- src/components/signIn/Form.jsx
+++ src/components/signIn/Form.jsx
@@ -5,6 +5,7 @@ import { updateField } from '../../redux/controls/user';
 
 function Form({ label, type, err, show, hide, x, name,forv }) {
     const user = useSelector((store) => store.user);
+    console.log(user);
     const dispatch = useDispatch();
     const [value, setValue] = useState("")
     const [status, setStatus] = useState(false)
@@ -27,7 +28,7 @@ function Form({ label, type, err, show, hide, x, name,forv }) {
     return (
         <div className="inp">
             <label style={{ color: value.length > 0 || blur ? "green" : "red" }} className={status || value.length > 0 ? 'inputOut' : "inputIn" || value.length > 0} htmlFor="">{label}</label>
-            <input style={{ border: value.length > 0 || blur ? "1px solid " : "1px solid red" }} ref={forv} value={value} name={name} type={view ? "text" : type} onBlur={changeBlur} onFocus={() => setStatus(true)} onChange={changeValue} />
+            <input style={{ border: value.length > 0 || blur ? "1px solid " : "1px solid red" }} ref={forv} value={value} name={name} type={view ? "text" : type} onBlur={changeBlur} onFocus={() => setStatus(true)} onChange={changeValue} aria-invalid />
             <p className='show' > <span onClick={() => setView(!view)}> {view ? show : hide}</span> <span style={{ display: !blur && !value.length > 0 ? "block" : "none" }}>{x}</span> </p>
             {value.length > 0 || blur ? "" : <p style={{ paddingLeft: "10px" }}>  <span style={{ fontSize: "20px", color: "red" }}>x</span> {err}</p>}
         </div>
