d420ec84986725b3343854b370dd5682ee55a550
vusalhasanov99 <vusqa1999@gmail.com>
2023-05-01 16:18:09 +0400

    allCategory added

--- src/components/menu/allProducts/AllProducts.jsx
+++ src/components/menu/allProducts/AllProducts.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import './AllProducts.css'
 import MenuItems from '../menuItems/MenuItems'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import AllCategory from './allCategory/AllCategory'
 
 function AllProducts() {
-
+  const location = useLocation()
   return (
-    <div className='d-flex allProducts'> 
+    <div className='d-flex allProducts'>
       <MenuItems />
-     <Outlet/>
- 
+      {location.pathname == "/menu/all" ?
+        <AllCategory /> :
+        <Outlet />
+      }
+
     </div>
   )
 }
